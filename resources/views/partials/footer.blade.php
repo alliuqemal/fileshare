ef68980809548c147558d38034089e33005ffc88
alliuqemal <36800086+alliuqemal@users.noreply.github.com>
2020-08-21 09:22:43 +0200

    1

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,3 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.0.5
-    </div>
-    <strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
-    reserved.
+   Copyright © 2020
 </footer>
