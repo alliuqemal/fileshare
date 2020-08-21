2130581ce455cdd77c289fc262ecce8bef953ec1
alliuqemal <36800086+alliuqemal@users.noreply.github.com>
2020-08-21 09:30:56 +0200

    Revert "0"
    This reverts commit 500ec0040942e6b4a5a9ae535e380abf991fc780.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,39 +1,52 @@
-
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-        </li>
-    </ul>
+    <form class="form-inline ml-3">
+        <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form>
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown user-menu">
-            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                <img src="../../dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2" alt="User Image">
-                <span class="d-none d-md-inline">{{request()->user()->name}}</span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <li class="user-header bg-primary">
-                    <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                    <p>
-                        {{ request()->user()->name }}
-                        <small>Member since {{ optional(request()->user()->created_at)->diffForHumans() }}</small>
-                    </p>
+
+        @guest
+
+            <li class="nav-item ">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                 </li>
-                <li class="user-footer">
-                    <a href="/profile/edit" class="btn btn-default btn-flat">Edit Profile</a>
-                    <a href="javascript:void(0);" class="btn btn-default btn-flat float-right"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Sign out
-                    </a>
-                </li>
-            </ul>
-        </li>
+            @endif
+        @endguest
+        @auth
+            <li class="nav-item dropdown user-menu">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                    <img src="../../dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2"
+                         alt="User Image">
+                    <span class="d-none d-md-inline">{{request()->user()->name}}</span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <li class="user-header bg-primary">
+                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <p>
+                            {{ request()->user()->name }}
+                            <small>Member since {{ optional(request()->user()->created_at)->diffForHumans() }}</small>
+                        </p>
+                    </li>
+                    <li class="user-footer">
+                        <a href="/profile/edit" class="btn btn-default btn-flat">Edit Profile</a>
+                        <a href="javascript:void(0);" class="btn btn-default btn-flat float-right"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Sign out
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        @endauth
     </ul>
 </nav>
